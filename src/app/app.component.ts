75a357c85edd48ad7ef3526d8b476e9287d45b49
Nuha <saifinuha1001@gmail.com>
2021-11-25 17:30:54 +0700

    update all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0 ;
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+  password = '';
+
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value)
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    } else if(isNaN(parsedValue)){
+      this.length = 0;
+    }
+  }
+  
+  onChangeUseLetters(){
+    this.useLetters = !this.useLetters;
+  }
+  
+  onChangeUseNumbers(){
+    this.useNumbers = !this.useNumbers;
+  }
+  
+  onChangeUseSymbols(){
+    this.useSymbols = !this.useSymbols;
+  }
+  
+  onButtonClick(){
+    this.password='';
+    this.generatePassword()
+    }
+
+  generatePassword(){
+    const numbers = '123456789012345678901234567890';
+    const letters = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+    const symbols = '!@#$%^&*()!@#$%^&*()!@#$%^&*()'
+  
+    let validChars = '';
+    if(this.useLetters){
+      validChars += letters;
+    }
+    if(this.useNumbers){
+      validChars += numbers;
+    }
+    if(this.useSymbols){
+      validChars += symbols;
+    }
+  
+    for (let i = 0; i < this.length; i++){
+      this.password += validChars.charAt(Math.floor(Math.random() * validChars.length))
+    }
+  }
 }
